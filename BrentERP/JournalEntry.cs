87aadc94c9fe6ff46e4dea44f361bc1abd9658bd
Jethro <arile.jethrobrent@gmail.com>
2023-07-05 09:24:29 +0800

    Add project files.

---
+++ BrentERP/JournalEntry.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrentERP
+{
+    internal class JournalEntry
+    {
+        private protected DateTime JEDate;
+        private protected double Debit;
+        private protected double Credit;
+        private protected int DebitAccountNumber;
+        private protected int CreditAccountNumber;
+        private protected int JENumber;
+        private protected string DebitAccountName;
+        private protected string CreditAccountName;
+        private protected string JEDescription;
+
+        internal JournalEntry(double debit, double credit, int debitAccountNumber, string debitAccountName, int creditAccountNumber ,string creditAccountName, string jEDescription, int jENumber)
+        {
+            try
+            {
+                DateTime JE = DateTime.Now;
+                CreditAccountNumber = creditAccountNumber;
+                DebitAccountNumber = debitAccountNumber;
+                DebitAccountName = debitAccountName;
+                CreditAccountName = creditAccountName;
+                JEDescription = jEDescription;
+                JENumber = jENumber;
+
+                if (debit == 0)
+                {
+                    throw new ArgumentException("Debit must not equal to 0!");
+                }
+                else if (credit == 0)
+                {
+                    throw new ArgumentException("Credit must not equal to 0!");
+                }
+                else if (debit != credit)
+                {
+                    throw new ArgumentException("Debit must be equal to Credit!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception on JE Class Constructor: {e}");
+            }
+            
+        }
+
+        // TODO: Add SQL Code to add journal entry details to the journal entry database
+        // TODO: Convert Debit, Credit, DebitAccountNo, CreditAccountNo, DebitAccountName, DebitAccountNo to Dict
+
+    }
+}
---
+++ BrentERP/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BrentERP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testje1 = new JournalEntry(10000, 10000, 01001, "Cash", 02012, "Cash Deposit", "To record deposit of cash for account number 101101523", 1);
+            // PLACEHOLDER FOR FUTURE CODE
+            // TODO: Create Account Number Class
+        }
+    }
+}
